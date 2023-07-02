02768fc68515c7783b2cbff843cab4ce9715edf4
shawnpong <e0970178@u.nus.edu>
2023-07-02 15:27:19 +0800

    server

--- FRONTEND/src/Variables.js
+++ FRONTEND/src/Variables.js
@@ -1,5 +1,5 @@
 export const variables = {
     // API_URL:"http://127.0.0.1:8000/"
     // API_URL:"http://121.7.123.129:8000/"
-    API_URL:"https://ezpcdjango.onrender.com/"
+    API_URL:"https://ezpcdjango.onrender.com/mymodel/"
 }
--- Startserver.bat
+++ Startserver.bat
@@ -1,4 +1,4 @@
 @echo off
 start "Backend" cmd /k call ".\.venv\Scripts\activate.bat" ^& cmd /c "cd /d .\BACKEND && python manage.py runserver 0.0.0.0:8000"
-start "Frontend" cmd /k call ".\.venv\Scripts\activate.bat" ^& cmd /c "cd /d .\FRONTEND && npm start"
+start "Frontend" cmd /k call ".\.venv\Scripts\activate.bat" ^& cmd /c "cd /d .\FRONTEND && npm run build"
 exit
