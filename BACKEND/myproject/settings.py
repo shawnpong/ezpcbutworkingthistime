191a5b3167cfaa36a78e4916c683646a1cab5b95
shawnpong <e0970178@u.nus.edu>
2023-07-02 17:13:28 +0800

    debuggers

--- BACKEND/myproject/settings.py
+++ BACKEND/myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wv9m^1h&=6xv46i%oi0g58vqqvv!_hxu6gz(#t^&c_hxr(5%a1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
